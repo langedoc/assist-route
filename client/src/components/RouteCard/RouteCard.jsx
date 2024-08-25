ee622b8eb3a7f1bb43eba038187fb4b59fcce8fd
yulia <langedoc@gmail.com>
2024-08-25 16:26:45 +0200

    refactor: RouteCard clickable

--- client/src/components/RouteCard/RouteCard.jsx
+++ client/src/components/RouteCard/RouteCard.jsx
@@ -1,10 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './RouteCard.css';
 import Sabadell from '../../assets/RoutesImages/Sabadell.svg';
+import { useDispatch } from 'react-redux';
+import { setSelectedRoute } from '../../store/routesSlice';
+import { setStopStudents } from '../../store/studentsSlice';
 
 export default function RouteCard({ route }) {
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setSelectedRoute(`${route.id}`));
+    dispatch(setStopStudents([]));
+  };
+
   return (
-    <div className={`card card-container ${route.type==='evening' ? 'evening' : 'morning'}`}>
+    <div
+      className={`card card-container ${route.type==='evening' ? 'evening' : 'morning'}`}
+      onClick = {handleClick}
+      style={{ cursor: 'pointer' }}
+    >
       <img src={Sabadell} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{route.name}</h5>
